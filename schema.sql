d98fb490b3ae992031ef7142da75003e202dedd3
Henry <hlin117@gmail.com>
2013-06-26 12:53:27 -0500

    updated the schema for the database

--- schema.sql
+++ schema.sql
@@ -4,67 +4,74 @@
  * object as well.
  */
 
+ /* Slight error: I meant to put varchar(255), not varchar(225). 
+  * But it shouldn't make much of a difference
+  */
+ 
 create table checklist {
-	checklist_tip text,
-	checklist_age int,
-	checklist_id key int,
-	checklist_category varchar(225),
-	checklist_completed bit
+	check_id key int,
+	check_tip text,
+	check_age int,
+	check_category varchar(225),
+	check_completed bit
 };
 
 create table rss {
+	rss_id int
 	rss_url varchar(225),
 	rss_title varchar(225),
 	rss_source varchar(225),
-	rss_id int
+
 
 };
 
 create table children {
+	child_id key int,
+	child_parentID int
 	child_age int,
 	child_color text, /* To store a 64 unsigned number for a color */
 	child_name varchar(30),
 	child_gender bit, /* 0 for male, 1 for female */
-	child_id int,
-	child_parentID int
+
 
 };
 
 create table tips {
-	tips_age int,
-	tips_category text,
-	tips_id int,
-	tips_title varchar(225)
+	tip_id key int,
+	tip_age int,
+	tip_category text, /* Maybe should've been varchar? */
+	tip_title varchar(225)
 
 };
 
 
 create table favorites {
-	favorites_type varchar(225),
-	favorites_id int,
-	favorites_userID int
+	fav_id key int,
+	fav_type varchar(20), /* type of object */
+	fav_userID int
 
 };
 
 create table users {
+	user_id key int,
 	user_name varchar(30),
-	user_id int,
 	user_email varchar(30),
 	user_password text,
-	user_phone text,
-	user_language varchar(30) /* ENG = English */
+	user_phone text, (nullable)
+	user_language varchar(20) /* ENG = English */
 
 };
 
 
 
-create table event {
-	event_title varchar(225),
+create table events {
+	event_id key int
 	event_date date,
 	event_time time,
-	event_place varchar(225),
 	event_url text,
+	event_place varchar(225),
+	event_title varchar(225),
 	event_sponsor varchar(225),
-	event_id int
+
 
 };
