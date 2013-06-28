560dff49c80bbc83a053e892839aa601d86ccf71
Henry <hlin117@gmail.com>
2013-06-27 21:50:21 -0500

    Prelim code for connecting to the server

---
+++ favoritedb.php
@@ -0,0 +1,10 @@
+<script type="type/javascript">
+
+// Function that returns an array of all of the favorite objects.
+// Returns a favHeat object.
+function getFavHeap(userID) {
+	
+
+}
+
+</script>
