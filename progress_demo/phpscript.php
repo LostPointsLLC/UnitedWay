abae6ef6f6ed85e800954b580ee4514cb6755d6d
Joshua <joshua.hyk@gmail.com>
2013-06-27 20:00:23 -0500

    working version of progress bar
    -progress bar able to get count of checked items from sql db
    -updates progressbar using that count value

--- progress_demo/phpscript.php
+++ progress_demo/phpscript.php
@@ -1,5 +1,5 @@
 <?php
-
+header('Content-type: application/javascript');
 //Defines the username, password and hostname
 $username = "sql312369";
 $password = "lR6!gD5%";
@@ -7,19 +7,23 @@ $hostname = "sql3.freesqldatabase.com";
 
 // Connects to your Database
 $dbhandle = mysql_connect($hostname, $username, $password)or die("Unable to connect to MySQL");
-echo "Connected to MySQL<br/>";
+//echo "Connected to MySQL<br/>";
 
 //selects a database to work with
 $selected = mysql_select_db('sql312369',$dbhandle) 
   or die("Could not select examples");
-echo "connected to db<br/>";
+//echo "connected to db<br/>";
 
 //execute the SQL query and fetches the records
 $result = mysql_query("SELECT COUNT(Checked) FROM child WHERE Checked = 1");
 $row = mysql_fetch_assoc($result);
-echo "Count = ";
-echo($row['COUNT(Checked)']); //the numerical value of count
+//echo "Count = ";
+//echo($row['COUNT(Checked)']); //the numerical value of count
 
+//setting content type using http headers
+/**echo '<script type="text/javascript">';
+echo $row['COUNT(Checked)'];
+echo '</script>';**/
 echo json_encode($row['COUNT(Checked)']);// outputs JSON object containing "3"
-exit();
+//exit();
 ?>
--- progress_demo/updateProgress.js
+++ progress_demo/updateProgress.js
@@ -1,14 +1,21 @@
 //function to update progress. function is called from the start in HTML
+
 function updateProgress() {
 	//need to parse the JSON object gotten from the phpscript. having some trouble with it.
-	/**$.post("./phpscript.php",function(data){
-    alert(data);
-  	});**/
+$.getJSON("http://localhost/progress_demo/phpscript.php",function(data){
+  alert(data);
+  var bar = document.getElementById("childProg");
+  bar.value = data;
+});
+//alert(mydata);
+}
 
-    alert("1");
-    var bar = document.getElementById("childProg");
+/**function updateProgressBar(data){
+	
+	myvar = data;
+
+	//alert(myvar);
+	var bar = document.getElementById("childProg");
     // updates the value of the progress bar
     bar.value +=1;
-	
-
-}
+}**/
