4ab36f2cef4afa30b1fe51d0390c902172bda344
Joshua Heng <joshua.hyk@gmail.com>
2013-06-28 01:53:49 -0500

    Removed problem comment(Solved)

--- progress_demo/updateProgress.js
+++ progress_demo/updateProgress.js
@@ -1,7 +1,7 @@
 //function to update progress. function is called from the start in HTML
 
 function updateProgress() {
-	//need to parse the JSON object gotten from the phpscript. having some trouble with it.
+
 $.getJSON("http://localhost/progress_demo/phpscript.php",function(data){
   alert(data);
   var bar = document.getElementById("childProg");
