3eb82ef338b641186570dec5018f03daa9f67624
Neha Shafizadah <nshafi3@gmail.com>
2013-06-29 15:31:21 -0500

    Updated Files for Login and Registration + Clean Up

---
+++ mobileLoginHandler.php
@@ -0,0 +1,29 @@
+<?php
+	/*
+		Handles all data requests from web/mobile application
+		
+	*/
+	require("mobileConnect.php");
+	// Prevent Cross Site Scripting Attacks
+    $user_email = strip_tags($_POST['pUser']);
+    $user_pass = strip_tags($_POST['pPass']);
+	
+	// Prevent SQL injections with prepared statements
+	// mySQL connection variable: $dbConnection 
+    $userQuery = $dbConnection ->prepare("SELECT * FROM users WHERE user_email= ? AND user_password= ?");
+    $userQuery->bind_param('ss', $user_email, $user_pass);
+    if ($userQuery->execute()){
+        $userQuery->store_result();
+        $userQuery->bind_result($column1, $column2, $column3, $column4, $column5, $column6, $column7);
+        $userQuery->fetch();
+        if ($userQuery->num_rows == 1) {
+			// Successful login, echo out parentID.
+			echo $column1;
+        }
+        else {
+			// Unsuccessful Login.
+            echo 0;
+        }
+    }
+
+?>
