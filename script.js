603d704c863544e697029d1654d2d364f80a8067
Jae Yun Song <song14@illinois.edu>
2013-06-30 16:23:19 -0500

    button function added

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+//<script src="getFavHeap.js"></script>
+
 //favorite object for testing
-function favObject(type, title, content)
+function favObject(type, title, content, url)
 {
 	this.type = type;
 	this.title = title;
 	this.content = content;
+	this.url = url;
 }
 
-var fav_1 = new favObject("News", "What not to eat", "you should not eat coin");
-var fav_2 = new favObject("News", "Chambana Mom fundraising", "We raised about 100 dollars");
+var fav_1 = new favObject("News", "What not to eat", "you should not eat coin", "https://google.com");
+var fav_2 = new favObject("News", "Chambana Mom fundraising", "We raised about 100 dollars", "https://yahoo.com");
 var fav_3 = new favObject("Tips", "Which book is good for you son?", "Nothing");
 var fav_4 = new favObject("Tips", "Make your child happy", "Laugh, laugh, and laugh");
 var fav_5 = new favObject("Events", "Buy your daughter a doll!", "Don't");
@@ -44,32 +47,29 @@ function displayFavorites(){
 	var tipPointer = document.getElementById("tipsHead");
 	var eventPointer = document.getElementById("eventsHead");
 
-	/* test for null
-	if(rssPointer == null){
-
-		alert("rssPointer is null!");
-	}
-
-	if(tipPointer == null){
-
-		alert("tipPointer is null!");
-	}
-
-	if(eventPointer == null){
-
-		alert("eventPointer is null!");
-	}
-	*/
-
 	//print out news
 	for(var i = 0; i < rssArray.length; i++) {
 
+		//create new div element
 		var rssElement = document.createElement("div");
-		var rssTitle = rssArray[i].title;
-		var rssTitleNode = document.createTextNode(rssTitle);
 
+		//button for rssElement
+		rssElement.onclick = function(){
+			alert("clicked");
+		};
+
+		//get the title		
+		var rssTitle = rssArray[i].title;
+		
+		//create textNode
+		var rssTitleNode = document.createTextNode(rssTitle);
+		
+		//rssElement.appendChild(rssUrlElement);
 		rssElement.appendChild(rssTitleNode);
 		rssPointer.parentNode.insertBefore(rssElement, rssPointer.nextSibling);
+
+		//move the pointer down so that the list would be in order
+		rssPointer = rssPointer.nextSibling;
 
 	}
 
@@ -77,24 +77,37 @@ function displayFavorites(){
 	for(var j = 0; j < tipArray.length; j++) {
 
 		var tipElement = document.createElement("div");
+
+		tipElement.onclick = function(){
+			alert("clicked");
+		};
+
 		var tipTitle = tipArray[j].title;
 		var tipTitleNode = document.createTextNode(tipTitle);
 
 		tipElement.appendChild(tipTitleNode);	
 		tipPointer.parentNode.insertBefore(tipElement, tipPointer.nextSibling);
 
+		tipPointer = tipPointer.nextSibling;
+
 	}
 
 	//print out events
 	for(var k = 0; k < eventArray.length; k++) {
 
-
 		var eventElement = document.createElement("div");
+
+		eventElement.onclick = function(){
+			alert("clicked");
+		};
+
 		var eventTitle = eventArray[k].title;
 		var eventTitleNode = document.createTextNode(eventTitle);
 
 		eventElement.appendChild(eventTitleNode);
 		eventPointer.parentNode.insertBefore(eventElement, eventPointer.nextSibling);
+
+		eventPointer = eventPointer.nextSibling;
 
 	}
 
