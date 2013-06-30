0c3b8f321fc8f88ede7d806a8d5022b6361ceaa5
Henry <hlin117@gmail.com>
2013-06-29 21:00:41 -0500

    Fixed getFavHeap - BUT WITH DECREASED PERFORMANCE
    I'll try making the code better later, but it works by making the AJAX
    calls asynchronous. Maybe I could multi thread this later.

--- Henry/fav_functions.js
+++ Henry/fav_functions.js
@@ -8,6 +8,7 @@ function getFavHeap(userID) {
 	var tipsArray = getTipsArray(datastring);
 	var eventsArray = getEventsArray(datastring);
 
+	
 	var output = new favHeap(rssArray, tipsArray, eventsArray);
 
 	return output;
@@ -15,11 +16,13 @@ function getFavHeap(userID) {
 
 function getRssArray(datastring) {
 
+	var rss_output_array;
 	/* Sends a request to the server to get RSS items */
 	$.ajax({
 		type: "POST",
 		url: "http://localhost/teseting/php/getRss.php", 
 		data: datastring,
+		async: false,
 		cache: false,
 		success: function(data) {
 		
@@ -27,24 +30,32 @@ function getRssArray(datastring) {
 			//element.innerHTML = data;
 			var rss_array = jQuery.parseJSON(data);			
 			
-			var rss_output_array = new Array();
+			rss_output_array = new Array();
 			
 			$.each(rss_array, function(index_of_row, row) {
 				rss_output_array.push(new rss(row));
 			});
 			
-			return rss_output_array;
+			rss_output_array[0].rss_id;
+			
+			rss_output_array;
 		}
 	});
+	
+	rss_output_array[0].rss_id;
+	return rss_output_array;
 }
 
 function getTipsArray(datastring) {
 
+	var tips_output_array;
+
 	/* Sends request to the server to get tips */
-	$.ajax({
+	return $.ajax({
 		type: "POST",
 		url: "http://localhost/teseting/php/getTips.php",
 		data: datastring,
+		async: false,
 		cache: false,
 		success: function(data) {
 		
@@ -52,7 +63,7 @@ function getTipsArray(datastring) {
 			//element.innerHTML = data;
 			var tips_array = jQuery.parseJSON(data);			
 			
-			var tips_output_array = new Array();
+			tips_output_array = new Array();
 			
 			$.each(tips_array, function(index_of_row, row) {
 				tips_output_array.push(new tip(row));
@@ -65,20 +76,22 @@ function getTipsArray(datastring) {
 
 function getEventsArray(datastring) {
 	
-
+	var events_output_array;
+	
 	/* Sends request to the server to get events */
-	$.ajax({
+	return $.ajax({
 		type: "POST",
 		url: "http://localhost/teseting/php/getEvents.php",
 		data: datastring,
 		cache: false,
+		async: false,
 		success: function(data) {
 		
 			//var element = document.getElementById('output');
 			//element.innerHTML = data;
 			var events_array = jQuery.parseJSON(data);			
 			
-			var events_output_array = new Array();
+			events_output_array = new Array();
 			
 			$.each(events_array, function(index_of_row, row) {
 				events_output_array.push(new event(row));
