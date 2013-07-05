12c8393c5017fb5f3b14c7250910ddde5b7c3935
Henry <hlin117@gmail.com>
2013-07-05 13:32:35 -0500

    Adding information for tips
    From antonio

---
+++ tips/tips.js
@@ -0,0 +1,62 @@
+//@Birth
+var Growth_birth = new Array();
+Growth_birth.push(" Sleep - Your infant’s sleep will be disorganized during her first six weeks, because she doesn’t yet know the difference between day and night.  At six weeks, her total sleep may be about 16 hours per day, including a stretch of four to six hours per night.")
+Growth_birth.push("Your child’s growth and development - The AAP says that your infant should achieve these milestones by the end of his first month:")
+Growth_birth.push("Movement: Makes jerky, quivering arm thrusts.  Brings hands up to eyes and mouth. Moves head from side to side while lying on stomach. Has strong reflex movements.")
+Growth_birth.push("Eyesight: Focuses 8 to 12 inches away. Eyes wander and occasionally cross. Prefers black and white or high contrast patterns. Prefers human faces to patterns.")
+Growth_birth.push("Hearing: has fully matured hearing. Recognizes some sounds.")
+Growth_birth.push("Smell and Touch: Recognizes the scent of his mother’s breast milk.")
+Growth_birth.push("You cannot spoil an infant, so be sure to go to him whenever he fusses or cries. When you answer him, he learns that you understand his signals and are going to take care of his needs.")
+
+var safety_birth = new Array();
+safety_birth.push("SIDS - Always place your baby on his back to sleep during naptime and at nighttime.  Keep toys and other small objects out of your baby’s crib to prevent choking. Make sure the mattress and bedding are firm and flat and fit perfectly into the crib without gaps between the crib walls.  Do not place your infant to sleep on soft surfaces such as waterbeds, sofas, soft mattresses, pillows, comforters or sheepskins. Also, keep toys and stuffed animals out of the crib.  They can smother your baby.  Dress your baby in as many layers of clothing as you would wear and keep the temperature in your baby’s room comfortable for an adult.  Do not smoke around your baby.");
+safety_birth.push("Take care of yourself. Make sure you get plenty of support from friends, family or community groups.  Don’t be afraid to ask for help. Call your doctor if your “baby blues” last more than two weeks.")
+safety_birth.push("General Safety - Never leave your baby unattended on any surface above the floor.  Don’t attach pacifiers or other objects to your baby or the crib with a cord, string or necklace of any type. Always support the baby’s head and neck when moving her body.  Never shake your baby.")
+safety_birth.push("In the Car - A properly installed, federally approved car seat every time she is in the car. The rear facing position. The back seat.")
+
+var playtime_birth = new Array();
+playtime_birth.push("Just as baby’s body depends on parents for food to grow, his brain depends on experiences to grow and develop. Talk, sing, read and play with your baby right from the start to help him learn.")
+
+var Health_birth = new Array();
+Health_birth.push("According to the AAP, breastfed babies generally eat more frequently than bottle-fed infants. Some breastfed newborns will need to nurse every two hours, others every three.  Formula-fed infants will need to eat every three to four hours. Remember to burp him after each feeding.")
+Health_birth.push("Bathing - Your infant’s umbilical stump will fall off 10 to 20 days after birth, leaving a small wound that will take a few days to heal. Until then, wipe your baby’s body with a clean, warm, wet washcloth instead of using a bathtub. Be sure to regularly wipe her face and hands and carefully clean her genitals")
+Health_birth.push("After the umbilical stump heals, you can give your baby a traditional water bath in the kitchen sink or plastic baby tub. Fill the tub with two to three inches of warm water. (Double check the water temperature before placing your baby in the tub.)  Gradually slip the baby into the tub using your hand to support her head and neck.  When using the sink, seat your baby on a washcloth and hold her under the arm to prevent slipping.  Pour cups of bath water over her instead of water directly from the spout.  Wash her with a gentle hair and body soap.  Wrap your baby in a towel and pat her dry.")
+Health_birth.push("Jaundice - Many newborns have a jaundiced or yellowish skin color, which may last the first week or two. Your baby should be examined for jaundice at the hospital, it is important that she be examined again between three and five days of age.")
+Health_birth.push("Sun Exposure - Do not expose your baby to direct sunlight. The risks or benefits of sunscreen use are not yet known for babies younger than six months of age. If your baby needs to be outdoors, discuss sunscreen use and other options with your pediatrician.")
+Health_birth.push("Cradle Cap – a dry, flaky, harmless scalp condition that usually goes away on its own after several months. Treatment: washing with water or gentle baby shampoo and brushing the hair regularly can help. If the problem continues, talk to your doctor about special shampoo to treat the condition.")
+
+//@2months
+var Growth_2months = new Array();
+Growth_2months.push("Learning begins at birth and your child at two months has been learning a lot!  He is already able to smile at you and tell you by crying if he’s hungry, cold or scared. At this stage, the most important factors in his healthy development are loving relationships with warm, responsive and dependable adult caregivers. It’s essential to make the most of this critical period in his growth by spending lots of time holding, cuddling, playing and reading with your child.")
+Growth_2months.push("Let your doctor know if your baby: Doesn’t notice his hands. Doesn’t smile at the sound of your voice. Doesn’t follow moving objects with his eyes. Doesn’t respond to loud noises.")
+Growth_2months.push("Sleep - Encourage nighttime sleep by putting your baby to sleep between 6 and 10pm, turning off lights and keeping the area quiet. Always but your baby to sleep on her back to reduce the risk of SIDS.")
+Growth_2months.push("Colic - Colic usually sets in between two weeks and three to four months of age. An infant with colic is an otherwise healthy baby who cries for more than three consecutive hours at a time,  three days a week, for three weeks or more. To console a colicky baby, try the following: Let your baby such on a pacifier, bottle, hand or wrist. Rock or swing your baby. Take her for a drive or stroll. Swaddle your baby by wrapping her securely in a blanket. Try to stay calm, since your baby can sense when you are stressed. Create shushing sounds or white noise by running a hair dryer, a fan, a vacuum cleaner, the shower/water faucet, or a recording of wind or surf. Hold your baby on her left side or stomach until she falls asleep. Be sure to roll her onto her back once she is asleep.")
+Growth_2months.push("Read to Your Child from Day One – Introduce cloth or board books to explore. Choose books with bright colored pictures, familiar objects or photos of  babies.")
+
+var safety_2months = new Array();
+safety_2months.push("Never shake or spank your baby. Shaking can cause brain damage.")
+safety_2months.push("Keep small objects that present a choking hazard away from your baby. (Anything that fits in a toilet paper tube is too small.)")
+safety_2months.push("Let your doctor or a friend know if you have signs of post-partum depression, including feelings of sadness, anxiety or restlessness.")
+safety_2months.push("In the Car- Car seat must be properly attached or secured.")
+safety_2months.push("Never leave you baby unattended anywhere near water. Babies can drown in less than one inch of water.")
+safety_2months.push("In the Crib - Empty the crib of blankets, pillows, and stuffed animals – any of these can smother the baby.")
+safety_2months.push("Avoid devices that maintain sleep positions, since many of these have not been tested for safety and none have been proven to decrease the risk of SIDS. Keep your baby’s room at a temperature that’s comfortable for adults, don’t make it too warm.")
+
+var playtime_2months = new Array();
+playtime_2months.push("Play together right from the start. Play allows your baby to explore and express herself, connect with other people and make sense of her surroundings. Make silly faces, tickle her body. You are your baby’s first toy. Slowly move objects in front of your baby and watch as he tracks them with his eyes.")
+playtime_2months.push("Sing Songs. Your voice will soothe your baby and the sounds and rhythms will teach her about words and language. Repeat your favorite songs and nursery rhymes again and again. Play fun music at play time, or relaxing rhythms to calm your baby at bedtime.")
+playtime_2months.push("Talk to your Baby - Babies learn by listening to you talk, so the more you talk to your baby, the better. Talk about what you see and what you are doing during everyday activities. Watch as he communicates through his coos, smiles, laughs and cries.  Respond to your baby’s sounds and give him time to respond to yours.")
+
+var Health_2months = new Array();
+Health_2months.push("At two months your baby should have another well visit. Until your baby is three months old, always call the doctor if he develops a rectal temperature of 100.4F or higher.")
+Health_2months.push("Create a Predictable World for Your Baby - Providing routines lets your baby know that he can depend on you, allowing him to spend less energy fussing over his needs and more time learning. Build routines that suit your style. Before bedtime you can: read a book, give a bath, talk to stuffed animals, hug and kiss, say “I love you.”")
+Health_2months.push("Provide a Warm and Loving Environment - A baby’s ability to learn depends upon her emotional well-being; a baby who is content and comfortable has an easier time learning about the world. An infant cries only to let you know that she needs your help. Since you cannot spoil an infant, pick up your baby whenever she cries.")
+Health_2months.push("Remember…Discipline is never appropriate for infants. They have no control over their actions and therefore should not be held accountable.")
+
+
+
+
+
+
+
+
