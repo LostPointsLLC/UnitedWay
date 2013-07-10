8f3120c4a0dcb14f3cf3941b067e9f86b0bfa5c5
Paul Kim <rackadone@gmail.com>
2013-07-09 22:34:31 -0500

    commit styling

--- tips/js/personal_cat.js
+++ tips/js/personal_cat.js
@@ -1,4 +1,22 @@
+
+
+
 function healthTips() {
 	sessionStorage.tCat = "health";
+	document.location.href = "personal_tips.html";
+}
+
+function growthTips() {
+	sessionStorage.tCat = "growth";
+	document.location.href = "personal_tips.html";
+}
+
+function safetyTips() {
+	sessionStorage.tCat = "safety";
+	document.location.href = "personal_tips.html";
+}
+
+function playTips() {
+	sessionStorage.tCat = "playtime";
 	document.location.href = "personal_tips.html";
 }
