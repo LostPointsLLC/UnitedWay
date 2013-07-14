33aa66035d19e4361a562b7cae37f87ba7b98021
Joshua <joshua.hyk@gmail.com>
2013-07-14 01:39:59 -0500

    clean up calendar folder.

--- calendar/blah blah/php/events.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include("mockeventsarray.php");
-	/*
-	Using mock events that are stored in an array in mockeventsarray.php
-	
-	*/
-	echo json_encode($events);
-?>
---
+++ calendar/js/generatecalendar.js
@@ -0,0 +1,14 @@
+/* Generates the calendar button within the daily tips page.*/
+function generateCalBtn() {
+	document.write("<div class='calbtnwrap'><a href='../calendar/maincalendar.html'><img id = \"calbtn\"src='images/calendar-button.png'/></a></div>");
+}
+
+/* Generates the div boxes for date, activity and events on the daily tips page in calendar*/
+function generateDailyActivity(){
+	document.write("<div id = 'day'></div><div id = 'month'></div><div id = 'activity'></div>");
+}
+
+/* Generates the div boxes for the main calendar*/
+function generateMainCal(){
+	document.write("<div id = 'calendar'></div><div id = 'activity'></div>");
+}
