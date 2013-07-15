cd393ecc0a5a58e8b7f35a6398f1b5e491df2caa
Joshua <joshua.hyk@gmail.com>
2013-07-15 00:39:03 -0500

    added feature to maincalendar page.
    - 1st click on any day on the main calendar displays the activity and
    events below the calendar.
    - 2nd click on the same day will bring it to the calendartip page

--- calendar/js/calendar.js
+++ calendar/js/calendar.js
@@ -1,11 +1,30 @@
+var counter = 0;    //counter to count how many times that particular date is clicked
+var prevdate = null;    //variable to store previous date selected
 //loads the calendar for the current month
 $('#calendar').datepicker({
         inline: true,
         firstDay: 1,
         showOtherMonths: true,
         dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        onSelect: function(datestr, inst){
-        	linkTopage(datestr);
+        onSelect: function(datestr){
+            var m = parseInt(datestr.substring(0,2))-1; //selected box month's int value from 0 to 11
+            var d = parseInt(datestr.substring(3,5))-1; //selected box day's int value from 0 to 30
+            //checks if it is the first time the date is selected
+            //checks if the second date selected equals the prev date selected
+            if(prevdate==null || prevdate==datestr) 
+            {
+                counter++;
+            }
+            else    //the second selected date is a different date, reset counter to 1
+            {
+                counter = 1;
+            }
+            if(counter==2) //user has selected the date consecutively. link to bigger calendar activitypage
+            {
+                linkTopage(datestr);
+            }
+            prevdate = datestr; //assigns the date selected to the prevdate variable
+            displayTE(m,d); //calls function to display the activity and event below the main calendar
         }
     });
 $(document).ready(function() {
@@ -14,9 +33,7 @@ $(document).ready(function() {
     var month = currentDate.getMonth();
    // updateDate(day,month);
    // updateEvent(day,month);
-   
-   	document.getElementById('activity').innerHTML = "Activity: <br>" + 
-    getActivity(day-1,month) + "<br><br>"+displayTE(month,day-1);
+   displayTE(month,day-1);
 });
 //function to go to calendar tip page
 function linkTopage(param)
@@ -44,7 +61,7 @@ function getActivity(day,month){
 }
 
 //function to display today's event below the main calendar
-function displayTE(m,d){
+function getTE(m,d){
 var val;
 $.ajax({
         type: "POST",
@@ -57,4 +74,10 @@ $.ajax({
         }
      });   
     return val;
+}
+
+function displayTE(m,d)
+{
+    document.getElementById('activity').innerHTML = "Activity: <br>" + 
+    getActivity(d,m) + "<br><br>"+getTE(m,d);
 }
