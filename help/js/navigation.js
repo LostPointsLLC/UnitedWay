6327122f7fb2188ba33dafdd29179090eb782e79
Henry <hlin117@gmail.com>
2013-07-22 23:01:04 -0500

    prototype help page

---
+++ help/js/navigation.js
@@ -0,0 +1,17 @@
+function faq() {
+
+	document.location.href = "help_faq.html";
+
+}
+
+
+function how() {
+	document.location.href = "help_howToUse.html";
+
+}
+
+
+function contact() {
+	document.location.href = "help_contactUs.html";
+
+}
