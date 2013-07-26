3d8f78d232014c3986b7d0f6506b93fddae79c44
Tiffy Ye <irsnooze@hotmail.com>
2013-07-25 23:02:36 -0500

    combinejs files

--- calendar/js/calendartip.js
+++ calendar/js/calendartip.js
@@ -1,60 +1,7 @@
 $(document).ready(function() {
-    updateDAE(sessionStorage.date);
-});
-
-//function to update page with current date, activity of the day and events of the day
-function updateDAE(param)
-{
-    var monthNames = [ "January", "February", "March", "April", "May", "June",
-		       "July", "August", "September", "October", "November", "December" ]
-    var monthIndex= parseInt(param,10);	//gets the numerical value of month
-    var monthText = monthNames[monthIndex-1];//retrieves the string value of that month.
+    param = sessionStorage.date;
     var day = parseInt(param.substr(3),10);
-    //Updates the chosen day, month and activity on the current page
-    document.getElementById('day-text').innerHTML = day;
-    document.getElementById('month-text').innerHTML = monthText;
-    //has to offset by 1 because array index starts at 0.
-    
-    document.getElementById('activity').innerHTML = "<span id='activity-head'>Activity</span> <br>" + 
-	getActivity(day-1,monthIndex-1) + "<br><br>"+getEvent(day-1,monthIndex-1);
-  
-    getLibraryEvent(day,monthIndex-1);
- 
-}
-
-//go back to home page
-function goHome(){
-	document.location.href = "../home/";
-}
-
-//go to help page
-function goHelp(){
-	document.location.href = "../help/Calendar.html";
-}
-
-//go back to calendar page
-function goToCalendar(){
-	document.location.href = "../calendar/maincalendar.html";
-}
-
-//returns the tip associated with the given day and month; month = 0(jan), 1(feb); day = 1,2..,31
-function getActivity(day,month){
-	return tipsArray[month][day];
-}
-
-//function to get and update the event of that day
-function getEvent(d,m){
-var val;
-$.ajax({
-        type: "POST",
-        url: "php/events.php",
-        async:false,
-        cache: false,
-        success: function(data){
-            var obj = jQuery.parseJSON(data);
-            val = "<span id='event-head'>Events</span> <br>" + obj[m][d];
-        }
-     });   
-    return val;
-}
+    var month = parseInt(param,10);
+    updateDate(day,month-1);
+});
 
--- calendar/js/todaysevent.js
+++ calendar/js/todaysevent.js
@@ -3,42 +3,10 @@ $(document).ready(function() {
 	var day = currentDate.getDate();
 	var month = currentDate.getMonth();
     updateDate(day,month);
-
 });
 
-//function to update page with current date, activity of the day and events of the day
-function updateDate(d,m){
-	var monthNames = [ "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December" ]
-	var monthText = monthNames[m];//retrieves the string value of that month.
-    var day = d;
-    //Updates the chosen day, month and activity on the current page
-    document.getElementById('day-text').innerHTML = day;
-    document.getElementById('month-text').innerHTML = monthText;
-    //has to offset by 1 because array index starts at 0.
-    document.getElementById('activity').innerHTML = "<span id='activity-head'>Activity</span> <br>" + getActivity(day-1,m) 
-	+ "<br><br>";
-	getLibraryEvent(d,m);
-}
 
-//go back to home page
-function goHome(){
-	document.location.href = "../home/";
-}
 
-//go to help page
-function goHelp(){
-	document.location.href = "../help/Calendar.html";
-}
 
-//go back to calendar page
-function goToCalendar(){
-	document.location.href = "../calendar/maincalendar.html";
-}
-
-//returns the tip associated with the given day and month; month = 0(jan), 1(feb); day = 1,2..,31
-function getActivity(day,month){
-	return tipsArray[month][day];
-}
 
 
