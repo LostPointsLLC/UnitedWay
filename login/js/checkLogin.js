37fee44a6c50aace75683448507aa95f0a669146
Joshua <joshua.hyk@gmail.com>
2013-07-29 16:12:06 -0500

    spanish support for login and reg

--- login/js/checkLogin.js
+++ login/js/checkLogin.js
@@ -33,23 +33,48 @@ function checkLogin(first, last, phone, email, pass) {
 
 	// Checks the first name
 	if(!checkAlpha(first)) {
+		if(sessionStorage.lang=="ENG"){
 		document.getElementById("result").innerHTML = "<p class='fail'>Registration failed. Your first name isn't all letters.</p>";
+		}
+		else{
+		document.getElementById("result").innerHTML = "<p class='fail'>Error en el registro. Su nombre no es todas las letras.</p>";
+		}
 		flag = false; 
 	}
 	if(!checkAlpha(last)) {
+		if(sessionStorage.lang=="ENG"){
 		document.getElementById("result").innerHTML = "<p class='fail'>Registration failed. Your last name isn't all letters.</p>";
+		}
+		else{
+		document.getElementById("result").innerHTML = "<p class='fail'>Error en el registro. Su apellido no es de todas las letras.</p>";
+		}
 		flag = false; 
 	}
 	if(!checkNumbers(phone)) {
+		if(sessionStorage.lang=="ENG"){
 		document.getElementById("result").innerHTML = "<p class='fail'>Registration failed. Your phone number isn't all numbers.</p>";
+		}
+		else{
+		document.getElementById("result").innerHTML = "<p class='fail'>Error en el registro. Su n&uacute;mero de telé&eacute;fono no es todos los n&uacute;meros.</p>";
+		}	
 		flag = false; 
 	}
 	if(!checkEmail(email)) {
+		if(sessionStorage.lang=="ENG"){
 		document.getElementById("result").innerHTML = "<p class='fail'>Registration failed. Your email address is invalid.</p>";
+		}
+		else{
+		document.getElementById("result").innerHTML = "<p class='fail'>Error en el registro. Su direcci&oacute;n de correo electr&oacute;nico no es v&aacute;lida.</p>";
+		}
 		flag = false; 
 	}
 	if(!pass) {
+		if(sessionStorage.lang=="ENG"){
 		document.getElementById("result").innerHTML = "<p class='fail'>Registration failed. Please type in a password.</p>";
+		}
+		else{
+		document.getElementById("result").innerHTML = "<p class='fail'>Error en el registro. Por favor, introduzca una contrase&ntilde;a.</p>";
+		}
 		flag = false; 
 	}
 
