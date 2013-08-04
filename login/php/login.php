b466d0326c99706b0f71ab566f3e91ecb815e16a
Paul Kim <rackadone@gmail.com>
2013-08-04 17:11:06 -0500

    added PHPASS encryption
    - secure password encryption

--- login/php/login.php
+++ login/php/login.php
@@ -1,17 +1,33 @@
 <?php
-	/*
-		Handles all data requests from web/mobile application
-		
-	*/
+	/**
+	 * php/Favorites.php
+	 * Author: Henry Lin (Minor additions: Paul Kim, update with PHPASS encryption)
+	 * PHPASS: http://www.openwall.com/phpass/
+	 * PHP script used by :
+	 *     login/js/registration.js
+	 *
+	 * Queries from tables: users
+	 * Updates tables: users
+	 */
+	 
 	require("../../php/connect.php");
+	require("../../php/phpass-0.3/PasswordHash.php");
 	// Prevent Cross Site Scripting Attacks
-    $user_email = strip_tags($_POST['pUser']);
-    $user_pass = strip_tags($_POST['pPass']);
+    $user_email = strip_tags(get_post_var('pUser'));
+    $user_pass = strip_tags(get_post_var('pPass'));
+	
+	// Base-2 logarithm of the iteration count used for password stretching
+	$hash_cost_log2 = 8;
+	// Do we require the hashes to be portable to older systems (less secure)?
+	$hash_portable = FALSE;
+	$hasher = new PasswordHash($hash_cost_log2, $hash_portable);
+	$hash = $hasher->HashPassword($user_pass);
+	unset($hasher);
 	
 	// Prevent SQL injections with prepared statements
 	// mySQL connection variable: $dbConnection 
     $userQuery = $dbConnection ->prepare("SELECT * FROM users WHERE user_email= ? AND user_password= ?");
-    $userQuery->bind_param('ss', $user_email, $user_pass);
+    $userQuery->bind_param('ss', $user_email, $hash);
     if ($userQuery->execute()){
         $userQuery->store_result();
         $userQuery->bind_result($column1, $column2, $column3, $column4, $column5, $column6, $column7);
@@ -28,4 +44,10 @@
 	
 	mysqli_close($dbConnection);
 
+	function get_post_var($var) {
+		$val = $_POST[$var];
+		if (get_magic_quotes_gpc())
+			$val = stripslashes($val);
+		return $val;
+	}
 ?>
