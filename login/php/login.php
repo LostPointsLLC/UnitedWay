167fff65129d7c2c4ecddb6f9dcc6269a67d9af6
Paul Kim <rackadone@gmail.com>
2013-08-04 18:38:10 -0500

    fixed login's index.html

--- login/php/login.php
+++ login/php/login.php
@@ -27,7 +27,7 @@
 	// Prevent SQL injections with prepared statements
 	// mySQL connection variable: $dbConnection 
     $userQuery = $dbConnection ->prepare("SELECT * FROM users WHERE user_email= ? AND user_password= ?");
-    $userQuery->bind_param('ss', $user_email, $hash);
+    $userQuery->bind_param('ss', $user_email, $user_pass);
     if ($userQuery->execute()){
         $userQuery->store_result();
         $userQuery->bind_result($column1, $column2, $column3, $column4, $column5, $column6, $column7); // We want to give the user $column1
