6cd478796ed03669a82ee777cbf6adfad162ed73
Joshua <joshua.hyk@gmail.com>
2013-08-09 04:31:45 -0500

    change lang feature added in settings
    - needs styling

--- js/langselect.js
+++ js/langselect.js
@@ -1,8 +1,4 @@
-//default language is English
-localStorage.lang = "ENG";
-
 function handleclick(myRadio) {
     //alert('Value: ' + myRadio.value);
     localStorage.lang = myRadio.value;
-    //alert(sessionStorage.lang);
 }
