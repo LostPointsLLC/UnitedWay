2cf42654ecc7cadfcfe8bba8e734c71e0a571866
Henry <hlin117@gmail.com>
2013-08-10 01:26:46 -0500

    so far i've failed, but calling it a night

--- favorites/js/initializeFavs.js
+++ favorites/js/initializeFavs.js
@@ -11,14 +11,33 @@ function initializeFavs() {
 	displayFavNews(favHeap.rssArray);
 	displayFavTips(favHeap.tipsArray);
 	displayFavEvents(favHeap.eventsArray);
+	
+	assignDataRoles();
 }
+
+function assignDataRoles() {
+
+	/* Assigns the "collapsible" data role to sections */
+	var sections = document.getElementsByClassName("section");
+	alert(sections);
+	for(var i = 0; i < sections.length; i++)
+		sections[i].setAttribute("data-role", "collapsible");
+
+	/* Assigns 'collapsible-set' to the id = set */
+	document.getElementById("set").setAttribute("data-role", "collapsible-set");
+	
+	/* Assigns 'content' to the id = content */
+	document.getElementById("content").setAttribute("data-role", "content");
+}
+
+
 
 /* Displays the favorited rss feeds using
  * the rssArray.
  */
 function displayFavNews(rssArray) {
 
-	var rssPointer = document.getElementById("news-feed-content");
+	var rssPointer = document.getElementById("news-feed");
 
 	/* Ideally, each div should look like this:
 	 *
@@ -37,7 +56,7 @@ function displayFavNews(rssArray) {
 		rssPointer.innerHTML += outputString;
 		return;
 	}	 
-	for(var i = 0; i < rssArray.length && i < fav_limit; i++) {
+	for(var i = 0; i < rssArray.length ; i++) {
 		var rss = rssArray[i];
 		var parity 		= (i % 2 == 0) ? "even" : "odd";
 		var last 	= (i == rssArray.length-1) ? "last-item" : "";
@@ -48,17 +67,6 @@ function displayFavNews(rssArray) {
 		outputString += "<div class='delete-box'><img class='delete' src='../images/remove-button-blkoutline.png' class='delete' onClick='unfavorite(" + rss.fav_id + ")'/></div>";
 		outputString += "</div>";
 	}
-	if(i < rssArray.length) {
-		var parity 		= (i % 2 == 0) ? "even" : "odd";
-		var last 	= "last-item";
-		outputString += "<div class='list-item " +  parity + " " + last + "' id='seeMore'>"
-		outputString += "<div class='item-text-box'>";
-		outputString += "<a>See More</a>";
-		outputString += "</div>";
-		outputString += "<div class='delete-box'></div>";
-		outputString += "</div>";		
-	
-	}
 	rssPointer.innerHTML += outputString;
 }
 
@@ -67,7 +75,7 @@ function displayFavNews(rssArray) {
  * tipsArray.
  */
 function displayFavTips(tipsArray) {
-	var tipsPointer = document.getElementById("tips-content");
+	var tipsPointer = document.getElementById("tips");
 
 	/* Ideally, each div should look like this:
 	 *
@@ -86,7 +94,7 @@ function displayFavTips(tipsArray) {
 		tipsPointer.innerHTML += outputString;
 		return;
 	}	
-	for(var i = 0; i < tipsArray.length && i < fav_limit; i++) {
+	for(var i = 0; i < tipsArray.length; i++) {
 		var tip = tipsArray[i];
 		var tipArray;
 		switch(tip.tip_category) { // Get tip content
@@ -117,19 +125,6 @@ function displayFavTips(tipsArray) {
 		outputString += "</div>";
 	}
 	
-	if(i < tipsArray.length) {
-		var parity 		= (i % 2 == 0) ? "even" : "odd";
-		var last 	= "last-item";
-		outputString += "<div class='list-item " +  parity + " " + last + "' id='seeMore'>"
-		outputString += "<div class='item-text-box'>";
-		outputString += "<a>See More</a>";
-		outputString += "</div>";
-		outputString += "<div class='delete-box'></div>";
-		outputString += "</div>";		
-	
-	}
-	
-	
 	
 	tipsPointer.innerHTML += outputString;
 
@@ -140,7 +135,7 @@ function displayFavTips(tipsArray) {
  */
 function displayFavEvents(eventsArray) {
 
-	var eventsPointer = document.getElementById("events-content");
+	var eventsPointer = document.getElementById("events");
 	var outputString = "";
 	/* Ideally, each div should look like this:
 	 *
