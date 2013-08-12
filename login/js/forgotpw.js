4e599c3077a4b7bfe46e6a44a3c6e4e6058c6897
Joshua <joshua.hyk@gmail.com>
2013-08-12 02:48:17 -0500

    forgot pw function working(email not working yet)

---
+++ login/js/forgotpw.js
@@ -0,0 +1,51 @@
+function forgotpw(){
+	//takes email address
+	var email = document.getElementById("email").value;
+	//checks if the email field is empty
+	if(email==""){
+		document.getElementById("result").innerHTML = "<p id='fail'>Please enter the email address you would like the temporary password to be sent to.</p>";
+		return;
+	}
+	//checks if the input email is valid
+	else if(!checkEmail(email)){
+		document.getElementById("result").innerHTML = "<p id='fail'>Please enter a valid email address for the temporary password to be sent to.</p>";
+		return;
+	}
+	//generates a random 8 char long pw
+	//source: http://www.latestcode.net/2012/12/javascript-random-password-generator.html
+	var temp_pw='';
+	var password_characters='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	for(var i=0;i<8;i++){ 
+		temp_pw+=password_characters.charAt(Math.floor(Math.random()*password_characters.length))
+	}
+	//alert(temp_pw);
+	var params = "pEmail=" + email + "&pPassword=" + temp_pw;
+	//alert(params);
+
+	$.ajax({
+		type: "POST",
+		url: "php/forgotpw.php",
+		data: params,
+		cache: false,
+		async: false,
+		success: function(data) {
+			var res = data.trim();
+			//alert(res);
+			//temp pw successfully sent to user email
+			if(res==-1){
+				document.getElementById("result").innerHTML = "A temporary password has been sent to your email.";
+			}
+			//email was not found on database
+			else{
+				document.getElementById("result").innerHTML = "Email was not found on database.";
+				return;
+			}
+		}
+	});
+
+	//saves it to send to user
+	//hash password
+	//change pw in database
+
+	//sends pw to user's email.
+}
---
+++ login/php/forgotpw.php
@@ -0,0 +1,58 @@
+<?php
+	require ("class.phpmailer.php");
+	require("../../php/connect.php");
+	require("../../php/phpass-0.3/PasswordHash.php");
+
+	$email = mysqli_real_escape_string($dbConnection, $_POST['pEmail']);
+	$temp_pw = strip_tags(get_post_var('pPassword'));
+
+	// Base-2 logarithm of the iteration count used for password stretching
+	$hash_cost_log2 = 8;
+	// Do we require the hashes to be portable to older systems (less secure)?
+	$hash_portable = FALSE;
+	$hasher = new PasswordHash($hash_cost_log2, $hash_portable);
+	$hash = $hasher->HashPassword($temp_pw);
+	unset($hasher);
+
+	//checks if the email exists in the database
+	$querystring = "SELECT user_email FROM users WHERE user_email = '$email'";
+	$result = mysqli_query($dbConnection, $querystring);
+	if(!$result) die("-2" . " Query was " . $querystring . "Error is: " . mysqli_error($dbConnection)); 
+
+	//user exists in database.
+	if(mysqli_fetch_array($result)) {
+		// step 1. hash the temp password and update it to the database
+		//updates the database with the temp password
+		($changeQuery = $dbConnection ->prepare("UPDATE users SET user_password = ? WHERE user_email = ?"))||fail('MySQL prepare', $db->error);
+		$changeQuery->bind_param('ss',$hash, $email)|| fail('MySQL bind_param', $db->error);
+		$changeQuery->execute()|| fail('MySQL execute', $db->error);
+		
+		// step 2. sends the unhashed password to the user's email
+		//server email details
+		/*$server_un = "joshua.hyk";
+		$domain = "gmail.com";
+		$server_email = $server_un.'@'.$domain;*/
+		$from = "joshua.hyk@gmail.com";
+		//recipients content and email
+		$to = $email;
+		$subject = "Forgot Password";
+		$headers = "MIME-Version: 1.0rn"; 
+    	$headers .= "Content-type: text/html; charset=iso-8859-1rn"; 
+		$headers .= "From: Your Site <".$from.">\r\n";
+        $message = "You have requested that you forgot your password.<br>
+                    Password: <b>".$temp_pw."<b>";
+        mail($to, $subject, $message, $headers);
+
+
+        // step 3. print out a statement saying a password has been sent using an echo value
+		echo -1;
+		mysqli_close($dbConnection);
+	}
+
+	function get_post_var($var) {
+		$val = $_POST[$var];
+		if (get_magic_quotes_gpc())
+			$val = stripslashes($val);
+		return $val;
+	}
+?>
