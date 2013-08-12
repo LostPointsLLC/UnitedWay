d1a4529277600c1b6c601b71de9168b444756ced
Paul Kim <rackadone@gmail.com>
2013-08-11 23:32:53 -0500

    fixed some bugs in settings
    there are still problems with configuring children though
    
    - The place holder of a child's birthday becomes yyyy-mm-dd
    which is not the format we want, and there is no code that checks user
    input for b-day. If an incorrect format is used, this breaks every page
    that uses the childJsonObject localStorage object.
    
    - Even though we enter the right format when editing children, for some
    reason another child with a wackjob b-day is formed.. new child entry is
    entered into local storage

--- settings/js/initializeSettings.js
+++ settings/js/initializeSettings.js
@@ -77,7 +77,7 @@ function listChildren(param) {
 			tableString[++i] = 		"</td>";
 			tableString[++i] =		"<td class='ageCell'><span id='age" + key + "' class='text white-text'>" + ageFormatted + "</span></td>";
 			tableString[++i] =		"<td class='editCell'><img id='edit" + key + "' class='edit' src='../images/wrench.jpg' onClick='editChild(" + key + ")'/></td>";
-			tableString[++i] =		"<td class='deleteCell'><img id='delete" + key + "' class='delete' src='../images/remove-button-blkoutline.png' onClick='deleteChild(&#39" + obj[key]["child_name"] + "&#39, " + key + ")' /></td>"
+			tableString[++i] =		"<td class='deleteCell'><img id='delete" + key + "' class='delete' src='../images/remove-button-blkoutline.png' onClick='deleteChild(&#39" + obj[key]["child_name"] + "&#39, &quot;" + key + "&quot;)' /></td>"
 			tableString[++i] = "</tr>";
 			tableString[++i] = "</table>";
 			anchor.innerHTML = tableString.join('');	// Makes this bigass array into one bigass string
