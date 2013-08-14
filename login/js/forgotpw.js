49a39b8794d9a5aec012a605bd7344b150ee01e8
Joshua Heng <joshua.hyk@gmail.com>
2013-08-14 01:30:51 -0500

    fixed the forgot pw bug on android

--- login/js/forgotpw.js
+++ login/js/forgotpw.js
@@ -1,3 +1,10 @@
+function checkEmail(email) {
+	// http://stackoverflow.com/a/46181/11236
+  
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
 function forgotpw(){
 	//takes email address
 	var email = document.getElementById("email").value;
